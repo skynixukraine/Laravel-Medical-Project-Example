8e69dba7bf18326f60f74d31604f64b551cec43d
Bogdan <bmaletskyi@gmail.com>
2019-12-05 22:45:34 +0200

    OH-17 add tables for new functionality

---
+++ database/migrations/2019_12_05_112044_create_locations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 60);
+            $table->string('address', 80);
+            $table->float('lat', 10, 6);
+            $table->float('lng', 10, 6);
+            $table->string('type', 30);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
