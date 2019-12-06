2c48e2e5c64d4819e0407180f1c21202f9102981
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2019-12-06 18:20:03 +0200

    OH-18 modify locations and doctors tables

---
+++ database/migrations/2019_12_06_151248_additional_columns_to_doctors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AdditionalColumnsToDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('doctors', function (Blueprint $table) {
+            $table->string('email')->after('last_name');
+            $table->boolean('is_active')->default(false)->after('description');
+            $table->string('password')->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('doctors', function (Blueprint $table) {
+            $table->dropColumn('email');
+            $table->dropColumn('is_active');
+            $table->dropColumn('password');
+        });
+    }
+}
