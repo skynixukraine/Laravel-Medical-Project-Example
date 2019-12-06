ed427ce55b5c1848cdb8951d0aa5ae810af69532
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2019-12-06 18:14:14 +0200

    OH-18 assign listener to DoctorRegistered event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -33,7 +32,10 @@ class EventServiceProvider extends ServiceProvider
         ],
         'Laravel\Passport\Events\AccessTokenCreated' => [
             'App\Listeners\DeleteOldTokens',
-        ]
+        ],
+        \App\Events\DoctorRegistered::class => [
+            \App\Listeners\DoctorRegistered::class,
+        ],
     ];
 
     /**
