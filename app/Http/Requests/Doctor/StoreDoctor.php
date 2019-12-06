437cd0966479f8cbe5117b1cc5a59c82d38de2a6
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2019-12-06 18:12:28 +0200

    OH-18 add request validation for doctor registration action

---
+++ app/Http/Requests/Doctor/StoreDoctor.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Doctor;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreDoctor extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'photo' => 'required|dimensions:min_width=256,min_height=256,max_width=540,max_height=540',
+            'prefix' => 'required|string|max:10',
+            'first_name' => 'required|string|max:20',
+            'last_name' => 'required|string|max:20',
+            'email' => 'required|email',
+            'description' => 'required|string|max:3000',
+            'region_id' => 'required|exists:regions,id',
+            'password' => 'required|string|min:6|max:255|confirmed',
+            'marker_name' => 'nullable|string|max:60',
+            'marker_address' => 'required|string|max:80',
+            'marker_lat' => 'nullable|numeric',
+            'marker_lng' => 'nullable|numeric',
+            'marker_type' => 'nullable|string|max:30',
+            'language_ids' => 'required|array',
+            'language_ids.*' => 'distinct|exists:languages,id',
+        ];
+    }
+}
