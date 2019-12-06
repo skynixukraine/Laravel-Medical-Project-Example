e4aacee304bda45a6b65ebd492e016d3a02e6a2e
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2019-12-06 18:13:14 +0200

    OH-18 add DoctorRegistered listener

---
+++ app/Listeners/DoctorRegistered.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Listeners;
+
+use App\Events\DoctorRegistered as DoctorRegisteredEvent;
+use App\Notifications\DoctorRegistered as DoctorRegisteredNotification;
+
+class DoctorRegistered
+{
+    /**
+     * Handle the event.
+     *
+     * @param  DoctorRegisteredEvent  $event
+     * @return void
+     */
+    public function handle(DoctorRegisteredEvent $event): void
+    {
+        $doctor = $event->getDoctor();
+
+        $doctor->notify(new DoctorRegisteredNotification($event->getDoctor()));
+    }
+}
