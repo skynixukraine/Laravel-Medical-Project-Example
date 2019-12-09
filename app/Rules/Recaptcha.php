4d8c0807e81ca48448236235389213268a308aae
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2019-12-09 10:38:55 +0200

    OH-18 add recaptcha rule

---
+++ app/Rules/Recaptcha.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class Recaptcha implements Rule
+{
+    private $action;
+
+    public function __construct(string $action)
+    {
+        $this->action = $action;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function passes($attribute, $value)
+    {
+        $response = (new \ReCaptcha\ReCaptcha(config('app.RECAPTCHA_SECRET_KEY')))
+            ->setExpectedAction($this->action)
+            ->verify($value, request()->ip());
+
+        return $response->isSuccess() && $response->getScore() < 0.6;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function message()
+    {
+        return trans('Recaptcha is invalid.');
+    }
+}
