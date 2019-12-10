ddad8b47efbb7dcb3537252404e013686edf7a10
Bogdan <bmaletskyi@gmail.com>
2019-12-10 22:56:20 +0200

    OH-18 add reset link routes

--- routes/api.php
+++ routes/api.php
@@ -48,6 +48,9 @@ Route::get('stripe/checkcreditcardstate',                        'StripeControll
 Route::get('stripe/app-checkout',                                'StripeController@appCheckout');  // creditcard payment for apps
 Route::post('stripe/webhook',                                    'StripeController@webhook');
 Route::post('register',                                          'API\DoctorController@register');
+Route::get('login',                                              'API\DoctorController@login');
+Route::post('send-reset-link',                                   'API\DoctorController@sendResetLinkEmail');
+Route::post('update-password',                                  'API\DoctorController@updatePassword')->name('password.update');
 
 //Route::middleware('auth:api')->get('/submission/open', function (Request $request) {
 //    return "hello ";
