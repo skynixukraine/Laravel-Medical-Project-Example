7297003d19b4a20a58cd6f4dc8da86b3de48add5
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2019-12-12 17:50:18 +0200

    OH-18 add nova resources

---
+++ app/Nova/Doctor.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\HasMany;
+use Laravel\Nova\Fields\HasOne;
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Image;
+use Laravel\Nova\Fields\Password;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
+
+class Doctor extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'App\Models\Doctor';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+            Image::make('Photo', 'photo'),
+            Text::make('Prefix', 'prefix'),
+            Text::make('First name', 'first_name'),
+            Text::make('Last name', 'last_name'),
+            Text::make('Email', 'email'),
+            Password::make('Password', 'password'),
+            Textarea::make('Description', 'description'),
+            Boolean::make('Is active', 'is_active'),
+            DateTime::make('Created at', 'created_at'),
+            DateTime::make('Updated at', 'updated_at'),
+            HasOne::make('Region'),
+            HasMany::make('Languages')
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}
