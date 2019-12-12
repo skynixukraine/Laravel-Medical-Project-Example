67a84950bf926bb79a40dc20d1f0fca526a71da7
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2019-12-12 17:19:17 +0200

    OH-18 add regions and languages get actions

---
+++ app/Http/Controllers/API/V1/LanguagesController.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Resources\LanguageResource;
+use App\Http\Resources\RegionResource;
+use App\Models\Language;
+use App\Models\Region;
+use Illuminate\Http\Request;
+use OpenApi\Annotations as OA;
+
+class LanguagesController extends ApiController
+{
+    /**
+     * @OA\Get(
+     *     tags={"Languages"},
+     *     path="/api/v1/languages",
+     *     summary="Get all languages",
+     *     description="Get all languages",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\MediaType(
+     *              mediaType="application/json",
+     *              @OA\Schema(
+     *                  properties={
+     *                      @OA\Property(
+     *                          property="data",
+     *                          @OA\Items(
+     *                              type="object",
+     *                              ref="#/components/schemas/LanguageResource"
+     *                          ),
+     *                      )
+     *                  }
+     *              )
+     *          )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal technical error was happened",
+     *         @OA\MediaType(
+     *              mediaType="application/json",
+     *              @OA\Schema(
+     *                  properties={
+     *                      @OA\Property(
+     *                          format="string",
+     *                          property="message",
+     *                          example="Something went wrong, please try again later."
+     *                      ),
+     *                  }
+     *              )
+     *          )
+     *      )
+     * )
+     */
+    public function index(Request $request)
+    {
+        return LanguageResource::collection(Language::all());
+    }
+}
