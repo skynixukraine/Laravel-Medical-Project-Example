5afd984c6066d1c1fb1b96a270c2e78b2b68ee3f
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2019-12-13 17:05:27 +0200

    OH-18 add doctors collection

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Language extends Model
 {
     public $timestamps = false;
 
     protected $fillable = ['code', 'name'];
+
+    public function doctors(): BelongsToMany
+    {
+        return $this->belongsToMany(Doctor::class);
+    }
 }
