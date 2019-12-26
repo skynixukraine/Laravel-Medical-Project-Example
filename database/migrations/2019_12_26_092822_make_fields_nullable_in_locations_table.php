3bbcfa9cb77839baba03c69c75fc31f73935cd09
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2019-12-26 11:07:05 +0200

    OH-18 add migration to set location's columns nullable

---
+++ database/migrations/2019_12_26_092822_make_fields_nullable_in_locations_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeFieldsNullableInLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('locations', function (Blueprint $table) {
+            $table->string('address')->nullable(true)->change();
+            $table->string('city')->nullable(true)->change();
+            $table->string('state')->nullable(true)->change();
+            $table->string('country')->nullable(true)->change();
+            $table->integer('postal_code')->nullable(true)->change();
+            $table->float('latitude', 10, 6)->nullable(true)->change();
+            $table->float('longitude', 10, 6)->nullable(true)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('locations', function (Blueprint $table) {
+            $table->string('address')->nullable(false)->change();
+            $table->string('city')->nullable(false)->change();
+            $table->string('state')->nullable(false)->change();
+            $table->string('country')->nullable(false)->change();
+            $table->integer('postal_code')->nullable(false)->change();
+            $table->float('latitude', 10, 6)->nullable(false)->change();
+            $table->float('longitude', 10, 6)->nullable(false)->change();
+        });
+    }
+}
