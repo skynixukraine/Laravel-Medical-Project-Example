46ecf3abb4455421292c3aefb826b282fe233ecc
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2020-01-03 15:39:30 +0200

    OH-70 remove unnecessary param

--- app/Http/Controllers/API/V1/DoctorController.php
+++ app/Http/Controllers/API/V1/DoctorController.php
@@ -781,13 +781,6 @@ class DoctorController extends ApiController
      *     path="/api/v1/doctors/{id}",
      *     summary="Get a doctor resource by id",
      *     description="Get a doctor resource by id",
-     *     @OA\Parameter(
-     *          name="id",
-     *          required=true,
-     *          description="A doctor's identificator",
-     *          in="query",
-     *          example="1"
-     *     ),
      *     @OA\Response(
      *         response=200,
      *         description="A doctor has been succesfully received",
