f899af5ac28132822f51c9b8fa10ec5ef73ace8b
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2020-01-09 17:15:45 +0200

    OH-69 add fetchValue method

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -8,16 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
+    public static $cached = [];
+
     public $timestamps = false;
-
-    public $incrementing = false;
-
-    protected $primaryKey = 'key';
 
     protected $fillable = ['key', 'value'];
 
-    public function getRouteKeyName()
+    public function __get($key)
     {
-        return 'key';
+        return parent::__get($key) ?? self::fetchValue($key);
+    }
+
+    public static function fetchValue($key)
+    {
+        return self::$cached[$key] ?? (self::$cached[$key] = self::where('key', $key)->first()->value ?? null);
     }
 }
