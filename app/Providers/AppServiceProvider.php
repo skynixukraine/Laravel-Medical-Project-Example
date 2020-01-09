bb9dfca39474d9c31cd621d4c694c5d2913fe1aa
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2020-01-09 16:20:45 +0200

    OH-69 config stripe

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
 use Illuminate\Support\ServiceProvider;
+use Stripe\Stripe;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Stripe::setApiKey(app('settings')->stripe_secret_key);
+        Stripe::setClientId(app('settings')->stripe_client_id);
     }
 
     /**
@@ -23,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('settings', static function ($app) {
+            return new Setting();
+        });
     }
 }
