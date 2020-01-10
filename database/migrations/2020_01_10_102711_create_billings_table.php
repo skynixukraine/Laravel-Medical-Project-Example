7b5f3260a57bbc569e4c0bc5d1dcc153b3cfd345
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2020-01-10 18:24:58 +0200

    OH-69 create billing table

---
+++ database/migrations/2020_01_10_102711_create_billings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBillingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('billings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('amount');
+            $table->string('currency', 20);
+            $table->unsignedInteger('enquire_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('enquire_id')
+                ->references('id')
+                ->on('enquires')
+                ->onDelete('SET NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('billings');
+    }
+}
