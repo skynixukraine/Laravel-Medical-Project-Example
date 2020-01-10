e52d9098fcb221d73e5a6700827f4d7bc106354f
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2020-01-10 18:44:06 +0200

    OH-69 add billings action

---
+++ app/Http/Controllers/API/V1/Doctor/Billings.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\API\V1\Doctor;
+
+use App\Http\Controllers\API\V1\ApiController;
+use App\Models\Doctor;
+use OpenApi\Annotations as OA;
+
+class Billings extends ApiController
+{
+    public function __invoke(Doctor $doctor)
+    {
+
+    }
+}
