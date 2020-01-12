9712d96be6f5f70cb59f4cba3aea80506d3713dc
Bogdan <bmaletskyi@gmail.com>
2020-01-12 22:54:31 +0200

    OH-69 implement billings list

--- app/Http/Controllers/API/V1/Doctor/Billings.php
+++ app/Http/Controllers/API/V1/Doctor/Billings.php
@@ -5,13 +5,19 @@ declare(strict_types=1);
 namespace App\Http\Controllers\API\V1\Doctor;
 
 use App\Http\Controllers\API\V1\ApiController;
+use App\Http\Resources\Billing;
 use App\Models\Doctor;
+use Illuminate\Http\Request;
 use OpenApi\Annotations as OA;
 
 class Billings extends ApiController
 {
-    public function __invoke(Doctor $doctor)
+    public function __invoke(Request $request, Doctor $doctor)
     {
-
+        return Billing::collection(
+            $doctor->billings()
+                ->orderBy('created_at', 'desc')
+                ->paginate($request->query('per_page', 50))
+        );
     }
 }
