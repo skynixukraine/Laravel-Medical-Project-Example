78a68e48503f32ddcdd1fac853cf4e086d703ea2
Bogdan <bmaletskyi@gmail.com>
2020-01-12 13:30:20 +0200

    OH-69 fix data

--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -13,13 +13,13 @@ class SettingsTableSeeder extends Seeder
     public function run()
     {
         DB::table('settings')->insert([
-            ['enquire_charge_description' => 'Enquire payment'],
-            ['enquire_admins_fee' => '15.15'],
-            ['enquire_price_currency' => 'usd'],
-            ['enquire_total_price' => '49.87'],
-            ['stripe_client_id' => 'ca_GVeaDvdqFNbcuNimN3M9c7Z9SLVCfd1X'],
-            ['stripe_secret_key' => 'sk_test_HmnLPA76TzcsQfNoLiKyvGdi000epjDg41'],
-            ['display_enquire_price' => '49.99$'],
+            ['key' => 'enquire_charge_description', 'value' => 'Enquire payment'],
+            ['key' => 'enquire_admins_fee', 'value' => '15.15'],
+            ['key' => 'enquire_price_currency', 'value' => 'usd'],
+            ['key' => 'enquire_total_price', 'value' => '49.87'],
+            ['key' => 'stripe_client_id', 'value' => 'ca_GVeaDvdqFNbcuNimN3M9c7Z9SLVCfd1X'],
+            ['key' => 'stripe_secret_key', 'value' => 'sk_test_HmnLPA76TzcsQfNoLiKyvGdi000epjDg41'],
+            ['key' => 'display_enquire_price', 'value' => '49.99$'],
         ]);
     }
 }
