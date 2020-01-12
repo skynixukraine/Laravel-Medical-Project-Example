79d2f8e22e4ce89313fa824a3c3e1700c892e0ed
Bogdan <bmaletskyi@gmail.com>
2020-01-12 14:19:59 +0200

    OH-69 add new notifications

---
+++ app/Notifications/DoctorResettedPassword.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications;
+
+use App\Traits\DoctorInfo;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class DoctorResettedPassword extends Notification
+{
+    use Queueable, DoctorInfo;
+
+    /**
+     * Get the notification's channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array|string
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Build the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage())
+            ->subject(Lang::getFromJson('Password changed'))
+            ->line(Lang::getFromJson('Your password was successfully changed.'));
+    }
+}
---
+++ app/Notifications/DoctorVerifiedEmail.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications;
+
+use App\Traits\DoctorInfo;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
+
+class DoctorVerifiedEmail extends Notification
+{
+    use Queueable, DoctorInfo;
+
+    /**
+     * Doctors will always be notified by Email
+     */
+    public function via()
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage())
+            ->subject(Lang::getFromJson('E-mail verified'))
+            ->line(Lang::getFromJson('Your e-mail was successfully verified. Now you can login.'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
