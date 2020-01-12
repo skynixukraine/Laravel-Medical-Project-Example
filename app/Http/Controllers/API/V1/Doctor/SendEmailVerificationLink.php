a3657f7cafcfb206117c5141579d5f27ca130dd0
Bogdan <bmaletskyi@gmail.com>
2020-01-12 19:57:29 +0200

    OH-69 improve validation

--- app/Http/Controllers/API/V1/Doctor/SendEmailVerificationLink.php
+++ app/Http/Controllers/API/V1/Doctor/SendEmailVerificationLink.php
@@ -5,20 +5,44 @@ declare(strict_types=1);
 namespace App\Http\Controllers\API\V1\Doctor;
 
 use App\Http\Controllers\API\V1\ApiController;
+use App\Http\Requests\Doctor\SendEmailVerificationLink as SendEmailVerificationLinkRequest;
 use App\Models\Doctor;
 use OpenApi\Annotations as OA;
 
 /**
- * @OA\Get(
+ * @OA\Post(
  *     tags={"Doctors"},
- *     path="/api/v1/doctors/{id}/send-email-verification-link",
+ *     path="/api/v1/doctors/send-email-verification-link",
  *     summary="Send verification email link",
  *     description="Send verification email link",
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\MediaType(
+ *              mediaType="application/x-www-form-urlencoded",
+ *              @OA\Schema(
+ *                  type="object",
+ *                  required={"email", "recaptcha"},
+ *                  @OA\Property(
+ *                      format="string",
+ *                      title="E-mail",
+ *                      description="A doctor's e-mail",
+ *                      property="email",
+ *                      example="doctor@gmail.com"
+ *                  ),
+ *                  @OA\Property(
+ *                      format="string",
+ *                      title="Recaptcha",
+ *                      description="A recaptcha token. An action must be 'send_email_verification_link'",
+ *                      property="recaptcha",
+ *                  )
+ *              )
+ *          )
+ *     ),
  *     @OA\Response(response=200, description="An e-mail has been sent"),
  *     @OA\Response(response=304, description="An e-mail already verified"),
  *     @OA\Response(
  *         response=404,
- *         description="Resource not found",
+ *         description="The doctor with provided email not found",
  *         @OA\MediaType(
  *              mediaType="application/json",
  *              @OA\Schema(
@@ -32,6 +56,41 @@ use OpenApi\Annotations as OA;
  *              )
  *          )
  *      ),
+ *     @OA\Response(
+ *         response=422,
+ *         description="An email is not valid",
+ *         @OA\MediaType(
+ *              mediaType="application/json",
+ *              @OA\Schema(
+ *                  title="Validation error",
+ *                  properties={
+ *                      @OA\Property(
+ *                          format="string",
+ *                          property="message",
+ *                          example="The given data was invalid."
+ *                      ),
+ *                      @OA\Property(
+ *                          property="errors",
+ *                          format="object",
+ *                          @OA\Property(
+ *                              property="email",
+ *                              @OA\Items(
+ *                                  type="string",
+ *                                  example="The email field is required."
+ *                              ),
+ *                          ),
+ *                          @OA\Property(
+ *                              property="recaptcha",
+ *                              @OA\Items(
+ *                                  type="string",
+ *                                  example="The recaptcha field is required."
+ *                              ),
+ *                          ),
+ *                      ),
+ *                  }
+ *              ),
+ *          )
+ *     ),
  *     @OA\Response(
  *         response=500,
  *         description="Internal technical error was happened",
@@ -52,8 +111,10 @@ use OpenApi\Annotations as OA;
  */
 class SendEmailVerificationLink extends ApiController
 {
-    public function __invoke(Doctor $doctor)
+    public function __invoke(SendEmailVerificationLinkRequest $request)
     {
+        $doctor = Doctor::whereEmail($request->email)->firstOrFail();
+
         abort_if($doctor->hasVerifiedEmail(), 304);
 
         $doctor->sendEmailVerificationNotification();
