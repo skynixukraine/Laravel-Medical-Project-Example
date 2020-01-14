6756dfa3f37f469f36e983cf6e819c14c5d0cf05
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2020-01-14 17:57:53 +0200

    OH-71 add enquire relation

--- app/Models/EnquireMessage.php
+++ app/Models/EnquireMessage.php
@@ -4,16 +4,23 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Events\EnquireMessageCreated;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EnquireMessage extends Model
 {
     public const SENDER_DOCTOR = 'DOCTOR';
     public const SENDER_PATIENT = 'PATIENT';
 
-    protected $fillable = ['content', 'enquire_message_id', 'sender'];
-
-    protected $casts = [
-        'enquire_message_id' => 'int'
+    protected $dispatchesEvents = [
+        'created' => EnquireMessageCreated::class
     ];
+
+    protected $fillable = ['content', 'sender', 'enquire_id'];
+
+    public function enquire(): BelongsTo
+    {
+        return $this->belongsTo(Enquire::class);
+    }
 }
