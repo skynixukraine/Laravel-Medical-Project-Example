0ea55491f4ba82c236efae02afe26706c250538f
Bogdan <bmaletskyi@gmail.com>
2020-01-22 14:51:03 +0200

    OH-71 remove stripe keys

--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -17,8 +17,6 @@ class SettingsTableSeeder extends Seeder
             ['key' => 'enquire_admins_fee', 'value' => '15.15'],
             ['key' => 'enquire_price_currency', 'value' => 'usd'],
             ['key' => 'enquire_total_price', 'value' => '49.87'],
-            ['key' => 'stripe_client_id', 'value' => 'ca_GVeaDvdqFNbcuNimN3M9c7Z9SLVCfd1X'],
-            ['key' => 'stripe_secret_key', 'value' => 'sk_test_HmnLPA76TzcsQfNoLiKyvGdi000epjDg41'],
             ['key' => 'display_enquire_price', 'value' => '49.99$'],
         ]);
     }
