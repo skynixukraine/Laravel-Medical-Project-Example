f46838eb402b5d8313605dc3c0deea11d05ef985
bogdan.maletskyi <bogdan.maletskyi@skynix.co>
2020-01-24 15:39:30 +0200

    OH-36 remove not existed trait

--- app/Models/Enquire.php
+++ app/Models/Enquire.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use App\Traits\HasApiTokensWithName;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
