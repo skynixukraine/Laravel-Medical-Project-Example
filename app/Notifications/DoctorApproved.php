b6fbc0126f09e1738b4a437b9ec38627158ee975
Kristina Antypova <kristina@skynix.co>
2020-04-01 12:33:21 +0300

    DoctorApproved.php edited online with Bitbucket

--- app/Notifications/DoctorApproved.php
+++ app/Notifications/DoctorApproved.php
@@ -28,9 +28,9 @@ class DoctorApproved extends QueueableNotification
     public function toMail($notifiable)
     {
         return $this->createMailMessage()
-            ->subject(__('Ihr Ärzteprofil wurde freigegeben und ist auf online-hautarzt.org abrufbar'))
-            ->greeting(__('Hallo,'))
-            ->line(__('Ihr Profil wurde freigegeben und kann auf online-hautarzt.de eingesehen werden.'));
+            ->subject(__('Your Doctor Profile is approved and is now visible on Online-Hautarzt.de site'))
+            ->greeting(__('Hello,'))
+            ->line(__('Your profile was approved and is accessible via online-hautarzt.de'));
     }
 
     /**
