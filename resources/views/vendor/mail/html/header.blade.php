af4b246f1d025d35632064effef80b18bc81deee
Alex <alexey.vnu@gmail.com>
2020-04-06 19:49:56 +0300

    OH-378-email-layout-update

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}">
-            {{ $slot }}
+            <img width="80" height="80" src="data:image/svg+xml;base64,{{base64_encode(file_get_contents(public_path() . '/images/favicondark.svg')) }}">
         </a>
     </td>
 </tr>
