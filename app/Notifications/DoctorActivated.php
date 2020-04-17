b4073d4710884c72672b5b1062964121951f5bd0
Alex <alexey.vnu@gmail.com>
2020-04-17 14:02:26 +0300

    OH-315-account-verified-successfully-email-v2

--- app/Notifications/DoctorActivated.php
+++ app/Notifications/DoctorActivated.php
@@ -33,8 +33,7 @@ class DoctorActivated extends QueueableNotification
             ->line(__('Dear colleague,'))
             ->line(__('We appreciate your interest!'))
             ->line(__('Your registration has been successfully verified. Now you can log in at online-hautarzt.de/login.'))
-            ->line(__('Please complete the information in your account: :account, in particular the billing details :billing and request approval from the administrator. You will enter the bank details with our payment provider Stripe, who will handle the automatic payment - so you will receive your remuneration automatically transferred to the bank account almost in real time. We cannot therefore activate this data before it is stored.',
-                ['account' => config('app.url') . '/account/personal-information', 'billing' => config('app.url') . '/account/billing']))
+            ->line(new HtmlString('Please complete the information in your <a href=\"/account/personal-information\"> Konto </a>, in particular the billing details :billing and request approval from the administrator. You will enter the bank details with our payment provider Stripe, who will handle the automatic payment - so you will receive your remuneration automatically transferred to the bank account almost in real time. We cannot therefore activate this data before it is stored.'))
             ->line(__('Please fill in the details in your profile and at the end click on “Apply for activation” at the bottom of the page as soon as you are finished.'))
             ->line(__('If you have any questions, simply send us an email to hilfe@online-hautarzt.de'))
             ->line(__('Kind regards and thank you'));
